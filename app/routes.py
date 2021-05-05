2ed16c5863846708a53c320ffba68bd17723834f
natannascimento <natan.graves@gmail.com>
2021-05-04 21:04:30 -0300

    Finalizado Capitulo 4

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect
+from flask.helpers import url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User, load_user
+from flask_login import logout_user
 
 @app.route('/')
 @app.route('/index')
@@ -17,11 +21,22 @@ def index():
         }
     ]
     return render_template('index.html',title='Home', user=user, posts = posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticaded:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('index')    
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))    
     return render_template('login.html', title ='Sing In', form = form)
+
+@app.route('/logout')
+def logout():
+    load_user()
+    return redirect(url_for('index'))
